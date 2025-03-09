928b6b6f339a31622445553a0c0724b2fe13fae4
Kushagra1480 <kushagrakartik1480@gmail.com>
2025-03-08 22:28:42 -0700

    Added gifs

--- words-weave/src/Header.jsx
+++ words-weave/src/Header.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import selectGif from './selecting-word.gif'
+import sendGif from './submit.gif'
+import litUrl from './lit.png'
 
 const Header = ({ gameData, formatDate }) => {
   const [showModal, setShowModal] = useState(false);
@@ -8,75 +11,81 @@ const Header = ({ gameData, formatDate }) => {
       <div className="md:hidden bg-gray-900 text-white sticky top-0 z-10 flex-shrink-0 rounded rounded-b-lg">
         <div className="p-4 flex justify-between items-center">
           <div>
-            <h1 className="text-xl font-bold">WordWeaves</h1>
+            <h1 className="text-xl font-bold">WordWeave</h1>
             <p className="text-sm">{formatDate()}</p>
           </div>
           <button 
             className="text-sm underline"
             onClick={() => setShowModal(true)}
           >
-            How to Play
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="mr-1">
+                    <circle cx="12" cy="12" r="10"/>
+                    <path d="M12 16v-4"/>
+                    <path d="M12 8h.01"/>
+                    </svg>
           </button>
           <span>Attempts: {gameData.attempts}</span>
         </div>
         <div className="p-6 text-center font-bold">
           <h2 className="text-2xl">
-            "
-            {gameData.quote?.split(/\s+/).map((word, index) => (
-              <span
-                key={index}
-                className={`
-                  ${gameData.progress[index] ? "text-green-850" : "text-white"}
-                  ${index ? "first-letter:float-left first-letter:text-4xl first-letter:font-bold first-letter:mr-1 first-letter:mt-1" : ""}
-                `}
-              >
-                {index > 0 && " "}{word}
-              </span>
-            ))}
-            "
+          "
+                {gameData.quote?.split(/\s+/).map((word, index) => (
+                    <span
+                    key={index}
+                    className={`
+                        ${gameData.progress[index] ? "text-green-700" : "text-white"}
+                        ${index ? "first-letter:float-left first-letter:text-4xl first-letter:font-bold first-letter:mr-1 first-letter:mt-1" : ""}
+                    `}
+                    >
+                    {index > 0 && " "}{word}
+                    </span>
+                ))}
+                "
           </h2>
           {gameData.author && <p className="text-sm mt-2">— {gameData.author}</p>}
         </div>
       </div>
   
-      <div className="hidden md:block bg-gray-900 text-white z-10 flex-shrink-0 rounded rounded-b-lg mt-3">
+      <div className="hidden md:block bg-gray-900 text-white z-10 flex-shrink-0 rounded rounded-b-lg mt-4">
         <div className="p-4 flex items-center">
-          <div className="flex-1">
+            <div className="flex-1">
             <h1 className="text-xl font-bold">WordWeave</h1>
             <p className="text-sm">{formatDate()}</p>
-          </div>
-          <div className="flex-1 text-center">
-            <button 
-              className="text-sm underline mx-auto hover:cursor-pointer"
-              onClick={() => setShowModal(true)}
-            >
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-info"><circle cx="12" cy="12" r="10"/><path d="M12 16v-4"/><path d="M12 8h.01"/></svg>
-              How to Play
-            </button>
-          </div>
-          <div className="flex-1 text-right">
-            <span>Attempts: {gameData.attempts}</span>
-          </div>
+            </div>
+            <div className="flex flex-col items-end p-2">
+                <button 
+                    className="text-sm underline flex items-center hover:cursor-pointer"
+                    onClick={() => setShowModal(true)}
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="mr-1">
+                    <circle cx="12" cy="12" r="10"/>
+                    <path d="M12 16v-4"/>
+                    <path d="M12 8h.01"/>
+                    </svg>
+                    How to Play
+                </button>
+                <span>Attempts: {gameData.attempts}</span>
+            </div>
         </div>
-        <div className="p-6 text-center font-bold">
-          <h2 className="text-3xl">
-            "
-            {gameData.quote?.split(/\s+/).map((word, index) => (
-              <span
-                key={index}
-                className={`
-                  ${gameData.progress[index] ? "text-green-700" : "text-white"}
-                  ${index ? "first-letter:float-left first-letter:text-4xl first-letter:font-bold first-letter:mr-1 first-letter:mt-1" : ""}
-                `}
-              >
-                {index > 0 && " "}{word}
-              </span>
-            ))}
-            "
-          </h2>
-          {gameData.author && <p className="text-sm mt-2">— {gameData.author}</p>}
+            <div className="p-6 text-center font-bold">
+                <h2 className="text-3xl">
+                "
+                {gameData.quote?.split(/\s+/).map((word, index) => (
+                    <span
+                    key={index}
+                    className={`
+                        ${gameData.progress[index] ? "text-green-700" : "text-white"}
+                        ${index ? "first-letter:float-left first-letter:text-4xl first-letter:font-bold first-letter:mr-1 first-letter:mt-1" : ""}
+                    `}
+                    >
+                    {index > 0 && " "}{word}
+                    </span>
+                ))}
+                "
+                </h2>
+                {gameData.author && <p className="text-sm mt-2">— {gameData.author}</p>}
+            </div>
         </div>
-      </div>
 
       {showModal && (
         <div className="fixed inset-0 bg-opacity-50 flex items-center justify-center z-50">
@@ -95,6 +104,19 @@ const Header = ({ gameData, formatDate }) => {
             </div>
             <div className="mt-2">
               Welcome to WordWeave! WordWeave is a word matching game where you're given a quote and paragraph and its your job to generate paragraphs that contain words from the quote using the words from the paragraph as prompts for an AI.
+              <h4>Here's how you play:</h4>
+              <span>
+                Select a word from the paragraph to form your prompt
+                <img src={selectGif}/> 
+              </span>
+              <span>
+                Once you're satisfied with your prompt hit send and watch your paragraph change
+                <img src={sendGif}/> 
+              </span>
+              <span>
+                The words in the quote light up if they're found in the generated response!
+                <img src={litUrl}/>
+              </span>
             </div>
           </div>
         </div>
